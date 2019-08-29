59f0fcc7e1989c6f49e0cb8468e4cd550d199a57
christakitos <xristakitos@gmail.com>
2019-08-29 19:24:45 +0300

    RTCPeerConnection added

--- js/main.js
+++ js/main.js
@@ -25,3 +25,87 @@ function handleLocalMediaStreamError(error) {
 // Initializes media stream.
 navigator.mediaDevices.getUserMedia(mediaStreamConstraints)
   .then(gotLocalMediaStream).catch(handleLocalMediaStreamError);
+
+let localPeerConnection;
+localPeerConnection = new RTCPeerConnection(servers);
+localPeerConnection.addEventListener('icecandidate', handleConnection);
+localPeerConnection.addEventListener(
+    'iceconnectionstatechange', handleConnectionChange);
+
+navigator.mediaDevices.getUserMedia(mediaStreamConstraints).
+  then(gotLocalMediaStream).
+  catch(handleLocalMediaStreamError);
+
+ function gotLocalMediaStream(mediaStream) {
+  localVideo.srcObject = mediaStream;
+  localStream = mediaStream;
+  trace('Received local stream.');
+  callButton.disabled = false;  // Enable call button.
+}
+
+localPeerConnection.addStream(localStream);
+trace('Added local stream to localPeerConnection.');
+
+function handleConnection(event) {
+  const peerConnection = event.target;
+  const iceCandidate = event.candidate;
+
+  if (iceCandidate) {
+    const newIceCandidate = new RTCIceCandidate(iceCandidate);
+    const otherPeer = getOtherPeer(peerConnection);
+
+    otherPeer.addIceCandidate(newIceCandidate)
+      .then(() => {
+        handleConnectionSuccess(peerConnection);
+      }).catch((error) => {
+        handleConnectionFailure(peerConnection, error);
+      });
+
+    trace(`${getPeerName(peerConnection)} ICE candidate:\n` +
+          `${event.candidate.candidate}.`);
+  }
+}
+
+trace('localPeerConnection createOffer start.');
+localPeerConnection.createOffer(offerOptions)
+  .then(createdOffer).catch(setSessionDescriptionError);
+
+
+// Logs offer creation and sets peer connection session descriptions.
+function createdOffer(description) {
+  trace(`Offer from localPeerConnection:\n${description.sdp}`);
+
+  trace('localPeerConnection setLocalDescription start.');
+  localPeerConnection.setLocalDescription(description)
+    .then(() => {
+      setLocalDescriptionSuccess(localPeerConnection);
+    }).catch(setSessionDescriptionError);
+
+  trace('remotePeerConnection setRemoteDescription start.');
+  remotePeerConnection.setRemoteDescription(description)
+    .then(() => {
+      setRemoteDescriptionSuccess(remotePeerConnection);
+    }).catch(setSessionDescriptionError);
+
+  trace('remotePeerConnection createAnswer start.');
+  remotePeerConnection.createAnswer()
+    .then(createdAnswer)
+    .catch(setSessionDescriptionError);
+}
+
+// Logs answer to offer creation and sets peer connection session descriptions.
+function createdAnswer(description) {
+  trace(`Answer from remotePeerConnection:\n${description.sdp}.`);
+
+  trace('remotePeerConnection setLocalDescription start.');
+  remotePeerConnection.setLocalDescription(description)
+    .then(() => {
+      setLocalDescriptionSuccess(remotePeerConnection);
+    }).catch(setSessionDescriptionError);
+
+  trace('localPeerConnection setRemoteDescription start.');
+  localPeerConnection.setRemoteDescription(description)
+    .then(() => {
+      setRemoteDescriptionSuccess(localPeerConnection);
+    }).catch(setSessionDescriptionError);
+}
